ba10478ff9e252c624ba35cad463674cde4631cb
Hola Bart <marcoferrusquia@gmail.com>
2023-10-11 20:02:17 -0600

    added changes to the src-sw

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,21 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  // we define the route for assets based on their destination
+  ({ request }) => request.destination === 'style' || request.destination === 'script' || request.destination === 'image',
+  // we use the StaleWhileRevalidate strategy for asset caching
+  new StaleWhileRevalidate({
+    cacheName: 'assets-cache', // here we set a cache name for assets
+    plugins: [
+      // the cache responses with status codes 0 and 200
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      // we set an expiration for the cached assets, like 7 days or something, you can change it
+      new ExpirationPlugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
